bc4a9bce7e1f52b0410f2c288bd8df9c752296ac
cristinac18 <c.castillo182011@gmail.com>
2025-03-13 08:51:06 -0600

    Create March_13.py

---
+++ March_13.py
@@ -0,0 +1,13 @@
+# day 6 challenge 
+# today i learned how to make a pazzword
+
+print("SECURE LOGIN")
+username = input("Username >")
+
+if username == "Cristina":
+  print("Welcome Cristina!")
+elif username == "Dora":
+  print("whats up dora!")
+elif username == "boots":
+  print("hi dora!")
+  print("Go away!")
